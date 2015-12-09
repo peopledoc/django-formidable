603761a3351f0bce027d4b37a1cad441bb79b1dd
Guillaume Camera <guillaume.camera@novapost.fr>
2015-12-09 15:52:43 +0100

    first try for models

--- formidable/models.py
+++ formidable/models.py
@@ -1,3 +1,22 @@
+# -*- coding: utf-8 -*-
 from django.db import models
 
-# Create your models here.
+
+class Formidable(models.Model):
+
+    label = models.CharField(max_length=256)
+    description = models.TextField()
+
+
+class Fieldidable(models.Model):
+    label = models.CharField(max_length=256)
+    form = models.ForeignKey(Formidable, related_name='fields')
+    type_id = models.CharField(max_length=256, choices=(('String', 'string'),))
+
+
+class Rulesidable(models.Model):
+    pass
+
+
+class Validationidable(models.Model):
+    pass
