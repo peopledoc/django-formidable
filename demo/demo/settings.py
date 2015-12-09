2bfa2e23aa94f85a5901bff3fb12f9bd16b2f25d
Guillaume Camera <guillaume.camera@novapost.fr>
2015-12-09 15:52:26 +0100

    added demo configuration to test

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
+    'formidable',
+    'rest_framework',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -100,3 +103,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = "/var/www/example.co"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
--- demo/demo/urls.py
+++ demo/demo/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+
 urlpatterns = [
     # Examples:
     # url(r'^$', 'demo.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^formidable/', include('formidable.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
