9122769c3ac6c8f05ece2b2cd4d4db0ecfcfecf4
Guillaume Camera <guillaume.camera@novapost.fr>
2015-12-16 17:21:29 +0100

    refs #7 ,added the url to create

--- formidable/urls.py
+++ formidable/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, url
 
-from formidable.views import FormidableDetail
+from formidable.views import FormidableDetail, FormidableUpdate
 
 urlpatterns = patterns(
     r'',
-    url(r'(?P<pk>\d+)/$', FormidableDetail.as_view()),
+    url(r'(?P<pk>\d+)/$', FormidableDetail.as_view(), name='form_detail'),
+    url(r'create/$', FormidableUpdate.as_view(), name='form_create'),
 )
