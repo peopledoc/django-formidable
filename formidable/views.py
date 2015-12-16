396532adc94304dbf56023b8a98514c562bccf77
Guillaume Camera <guillaume.camera@novapost.fr>
2015-12-16 17:21:38 +0100

    refs #7, added the view to create

--- formidable/views.py
+++ formidable/views.py
@@ -1,4 +1,6 @@
-from rest_framework.generics import RetrieveAPIView
+# -*- coding: utf-8 -*-
+
+from rest_framework.generics import RetrieveAPIView, CreateAPIView
 
 from formidable.models import Formidable
 from formidable.serializers import FormidableSerializer
@@ -8,3 +10,15 @@ class FormidableDetail(RetrieveAPIView):
 
     queryset = Formidable.objects.all()
     serializer_class = FormidableSerializer
+
+    def get_queryset(self):
+        qs = super(FormidableDetail, self).get_queryset()
+        return qs.prefetch_related('fields')
+
+
+class FormidableUpdate(CreateAPIView):
+    queryset = Formidable.objects.all()
+    serializer_class = FormidableSerializer
+
+    def create(self, *args, **kwargs):
+        return super(FormidableUpdate, self).create(*args, **kwargs)
