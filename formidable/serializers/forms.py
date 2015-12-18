fe95f07fb194cff267e9caf5149edd321c5b82df
Guillaume Camera <guillaume.camera@novapost.fr>
2015-12-18 14:27:43 +0100

    refs #10, review create and update  form serializer

--- formidable/serializers/forms.py
+++ formidable/serializers/forms.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from django.utils.functional import cached_property
 
 from rest_framework import serializers
 
@@ -16,14 +17,18 @@ class FormidableSerializer(serializers.ModelSerializer):
         depth = 2
 
     def create(self, validated_data):
-        fields_kwargs = None
-        if 'fields' in validated_data:
-            fields_kwargs = validated_data.pop('fields')
-
+        fields_kwargs = validated_data.pop('fields')
         form = Formidable.objects.create(**validated_data)
-
-        if fields_kwargs:
-            field_serializer = self.fields['fields']
-            field_serializer.create(fields_kwargs, form.id)
-
+        self.fields_serializer.create(fields_kwargs, form.id)
         return form
+
+    def update(self, instance, validated_data):
+        field_kwargs = validated_data.pop('fields')
+        super(FormidableSerializer, self).update(instance, validated_data)
+        self.fields_serializer.update(
+            instance.fields, field_kwargs
+        )
+
+    @cached_property
+    def fields_serializer(self):
+        return self.fields['fields']
