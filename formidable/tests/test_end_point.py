3f151423c95530a70ec8794c8cf31daeab87fe05
Guillaume Camera <guillaume.camera@novapost.fr>
2016-01-05 15:08:05 +0100

    Organize tests

---
+++ formidable/tests/test_end_point.py
@@ -0,0 +1,359 @@
+# -*- coding: utf-8 -*-
+import copy
+
+from django.test import TestCase
+
+from formidable.models import Formidable
+from formidable.serializers.forms import FormidableSerializer
+from formidable.serializers.fields import BASE_FIELDS, FieldSerializerRegister
+
+
+class RenderSerializerTestCase(TestCase):
+
+    def setUp(self):
+        self.form = Formidable.objects.create(
+            label=u'testform', description=u'test form',
+        )
+        self.text_field = self.form.fields.create(
+            type_id='text', label='test text',
+            placeholder='put your name here', helptext=u'your name',
+        )
+        self.text_field.accesses.create(
+            level=u'required', access_id=u'padawan'
+        )
+        self.serializer = FormidableSerializer(instance=self.form)
+
+    def test_ok(self):
+        self.assertTrue(self.serializer.data)
+
+    def test_register(self):
+        register = FieldSerializerRegister.get_instance()
+        assert len(register) == 11
+
+    def test_form_field(self):
+        data = self.serializer.data
+        self.assertIn('label', data)
+        self.assertEquals(data['label'], u'testform')
+        self.assertIn('description', data)
+        self.assertEquals(data['description'], u'test form')
+
+    def test_fields(self):
+        data = self.serializer.data
+        self.assertIn('fields', data)
+        self.assertEquals(len(data['fields']), 1)
+
+    def test_base_field_for_fieldidable(self):
+        field_text = self.serializer.data['fields'][0]
+        for field in BASE_FIELDS:
+            self.assertIn(field, field_text)
+
+    def test_text_field(self):
+        data = self.serializer.data
+        text_field = data['fields'][0]
+        self.assertEquals(text_field['type_id'], u'text')
+        self.assertEquals(text_field['label'], u'test text')
+        self.assertEquals(text_field['placeholder'], 'put your name here')
+        self.assertEquals(text_field['helptext'], 'your name')
+        self.assertEquals(text_field['default'], None)
+        self.assertNotIn('multiple', text_field)
+        self.assertNotIn('items', text_field)
+        self.assertIn('accesses', text_field)
+        self.assertEquals(len(text_field['accesses']), 1)
+        accesses = text_field['accesses'][0]
+        self.assertEquals(accesses['access_id'], u'padawan')
+        self.assertEquals(accesses['level'], u'required')
+
+    def test_dropdown_field(self):
+        self.form.fields.all().delete()
+        self.dropdown = self.form.fields.create(
+            type_id='dropdown', label=u'choose your weapon',
+        )
+        self.dropdown.items.create(key='tutu', value='toto')
+        self.dropdown.items.create(key=u'plop', value=u'Intérnätiônal')
+        serializer = FormidableSerializer(instance=self.form)
+        data = serializer.data['fields'][0]
+        for field in BASE_FIELDS:
+            self.assertIn(field, data)
+        self.assertIn('multiple', data)
+        self.assertEquals(data['multiple'], False)
+        self.assertIn('items', data)
+        self.assertEquals(len(data['items'].keys()), 2)
+        self.assertEquals(len(data['items'].values()), 2)
+
+
+class CreateSerializerTestCase(TestCase):
+
+    data = {
+        'label': u'test_create',
+        'description': u'description create',
+        'fields': []
+    }
+    fields_without_items = [
+        {
+            'slug': 'text_input', 'label': 'text label', 'type_id': 'text',
+            'accesses': [{'access_id': 'padawan', 'level': 'required'}]
+        }
+    ]
+
+    fields_with_items = [
+        {
+            'type_id': 'dropdown',
+            'slug': 'dropdown-input', 'label': 'dropdown label',
+            'multiple': False, 'items': {
+                'tutu': 'toto',
+                'tata': 'plop',
+            },
+            'accesses': [{
+                'access_id': 'padawan', 'level': 'required'
+            }]
+        }
+    ]
+
+    fields_with_validation = [
+        {
+            'slug': 'text_input',
+            'label': 'text label',
+            'type_id': 'text',
+            'accesses': [{'access_id': 'padawan', 'level': 'required'}],
+            'validations': [
+                {
+                    'type': 'minlength',
+                    'value': '5',
+                },
+            ]
+        }
+    ]
+
+    def test_create_form(self):
+        serializer = FormidableSerializer(data=self.data)
+        self.assertTrue(serializer.is_valid())
+        instance = serializer.save()
+        self.assertEquals(instance.label, u'test_create')
+        self.assertEquals(instance.description, u'description create')
+        self.assertEquals(instance.fields.count(), 0)
+
+    def test_create_field(self):
+        data = copy.deepcopy(self.data)
+        data['fields'] = self.fields_without_items
+        serializer = FormidableSerializer(data=data)
+        self.assertTrue(serializer.is_valid())
+        instance = serializer.save()
+        self.assertEquals(instance.label, u'test_create')
+        self.assertEquals(instance.description, u'description create')
+        self.assertEquals(instance.fields.count(), 1)
+        field = instance.fields.first()
+        self.assertEquals(field.type_id, 'text')
+        self.assertEquals(field.label, 'text label')
+        self.assertEquals(field.slug, 'text_input')
+        self.assertEquals(field.items.count(), 0)
+        # just one access has been specified, check the the other are created
+        # with default value
+        self.assertEquals(field.accesses.count(), 4)
+
+    def test_create_field_with_validations(self):
+        data = copy.deepcopy(self.data)
+        data['fields'] = self.fields_with_validation
+        serializer = FormidableSerializer(data=data)
+        self.assertTrue(serializer.is_valid())
+        instance = serializer.save()
+        self.assertEquals(instance.fields.count(), 1)
+        field = instance.fields.first()
+        self.assertEquals(field.validations.count(), 1)
+
+    def test_create_field_error_validations(self):
+        data = copy.deepcopy(self.data)
+        fields_data = copy.deepcopy(self.fields_with_validation)
+        fields_data[0]['validations'][0]['value'] = 'test'
+        data['fields'] = fields_data
+        serializer = FormidableSerializer(data=data)
+        self.assertFalse(serializer.is_valid())
+
+    def test_create_field_with_items(self):
+        data = copy.deepcopy(self.data)
+        data['fields'] = self.fields_with_items
+        serializer = FormidableSerializer(data=data)
+        self.assertTrue(serializer.is_valid())
+        instance = serializer.save()
+        self.assertEquals(instance.label, u'test_create')
+        self.assertEquals(instance.description, u'description create')
+        self.assertEquals(instance.fields.count(), 1)
+        field = instance.fields.first()
+        self.assertEquals(field.type_id, 'dropdown')
+        self.assertEquals(field.label, 'dropdown label')
+        self.assertEquals(field.slug, 'dropdown-input')
+        self.assertEquals(field.items.count(), 2)
+        self.assertTrue(
+            field.items.filter(key='tutu', value='toto').exists()
+        )
+        self.assertTrue(
+            field.items.filter(key='tata', value='plop').exists()
+        )
+        self.assertEquals(field.accesses.count(), 4)
+
+    def test_create_field_without_items(self):
+        data = copy.deepcopy(self.data)
+        fields = copy.deepcopy(self.fields_with_items)
+        fields[0].pop('items')
+        data['fields'] = fields
+        serializer = FormidableSerializer(data=data)
+        self.assertFalse(serializer.is_valid())
+        self.assertIn('fields', serializer.errors)
+
+    def test_create_wrong_access(self):
+        data = copy.deepcopy(self.data)
+        fields = copy.deepcopy(self.fields_with_items)
+        fields[0]['accesses'][0]['access_id'] = u'wrong'
+        data['fields'] = fields
+        serializer = FormidableSerializer(data=data)
+        self.assertFalse(serializer.is_valid())
+        self.assertIn('fields', serializer.errors)
+
+
+class UpdateFormTestCase(TestCase):
+
+    data = {
+        'label': u'edited form',
+        'description': 'description edited',
+        'fields': [],
+    }
+
+    fields = [
+        {'type_id': 'text', 'label': 'edited field', 'slug': 'text-slug',
+         'accesses': [{'access_id': 'padawan', 'level': 'required'}]}
+    ]
+
+    fields_items = [{
+        'type_id': 'dropdown', 'label': 'edited field',
+        'slug': 'dropdown-input', 'items': {
+            'gun': u'desert-eagle',
+            'sword': u'Andúril',
+        },
+        'accesses': [{'access_id': 'padawan', 'level': 'required'}],
+    }]
+
+    def setUp(self):
+        super(UpdateFormTestCase, self).setUp()
+        self.form = Formidable.objects.create(
+            label=u'testform', description=u'test form',
+        )
+
+    def test_update_simple(self):
+        serializer = FormidableSerializer(instance=self.form, data=self.data)
+        self.assertTrue(serializer.is_valid())
+        form = serializer.save()
+        self.assertEquals(form.pk, self.form.pk)
+        self.assertEquals(form.label, u'edited form')
+
+    def test_create_field_on_update(self):
+        data = copy.deepcopy(self.data)
+        data['fields'] = self.fields
+        serializer = FormidableSerializer(instance=self.form, data=data)
+        self.assertTrue(serializer.is_valid())
+        form = serializer.save()
+        self.assertEquals(form.pk, self.form.pk)
+        self.assertEquals(form.fields.count(), 1)
+        field = form.fields.first()
+        self.assertEquals(field.type_id, 'text')
+        # check accesses are fully created
+        self.assertEquals(field.accesses.count(), 4)
+
+    def test_create_items_on_update(self):
+        self.dropdown_fields = self.form.fields.create(
+            slug='dropdown-input', type_id='dropdown', label=u'weapons',
+        )
+        self.dropdown_fields.accesses.create(
+            access_id='padawan', level='required'
+        )
+        data = copy.deepcopy(self.data)
+        data['fields'] = copy.deepcopy(self.fields_items)
+        serializer = FormidableSerializer(instance=self.form, data=data)
+        self.assertTrue(serializer.is_valid())
+        form = serializer.save()
+        self.assertEquals(form.pk, self.form.pk)
+        self.assertEquals(form.fields.count(), 1)
+        field = form.fields.first()
+        self.assertEquals(field.items.count(), 2)
+        self.assertTrue(
+            field.items.filter(key=u'sword', value=u'Andúril').exists()
+        )
+        self.assertTrue(
+            field.items.filter(key=u'gun', value=u'desert-eagle').exists()
+        )
+
+    def test_update_fields(self):
+        self.text_field = self.form.fields.create(
+            type_id='text', label='test text', slug='text-slug',
+            placeholder='put your name here', helptext=u'your name',
+        )
+        self.text_field.accesses.create(
+            access_id='padawan', level='required'
+        )
+        data = copy.deepcopy(self.data)
+        data['fields'] = self.fields
+        serializer = FormidableSerializer(instance=self.form, data=data)
+        self.assertTrue(serializer.is_valid())
+        form = serializer.save()
+        self.assertEquals(form.pk, self.form.pk)
+        field = form.fields.first()
+        self.assertEquals(self.text_field.pk, field.pk)
+        self.assertEquals(field.label, u'edited field')
+
+    def test_update_fields_items(self):
+        self.dropdown_fields = self.form.fields.create(
+            slug='dropdown-input', type_id='dropdown', label=u'weapons',
+        )
+        self.dropdown_fields.accesses.create(
+            access_id='padawan', level='editable'
+        )
+        self.dropdown_fields.items.create(key=u'gun', value=u'eagle')
+        self.dropdown_fields.items.create(key=u'sword', value=u'excalibur')
+        data = copy.deepcopy(self.data)
+        data['fields'] = self.fields_items
+        serializer = FormidableSerializer(instance=self.form, data=data)
+        self.assertTrue(serializer.is_valid())
+        form = serializer.save()
+        self.assertEquals(form.pk, self.form.pk)
+        field = form.fields.first()
+        self.assertEquals(self.dropdown_fields.pk, field.pk)
+        self.assertEquals(field.label, u'edited field')
+        self.assertEquals(field.items.count(), 2)
+        self.assertTrue(
+            field.items.filter(key='gun', value='desert-eagle').exists()
+        )
+        self.assertTrue(
+            field.items.filter(key='sword', value=u'Andúril').exists()
+        )
+        qs = field.accesses.filter(access_id='padawan', level=u'required')
+        self.assertTrue(qs.exists())
+
+    def test_delete_on_update(self):
+        self.dropdown_fields = self.form.fields.create(
+            slug='dropdown-input', type_id='dropdown', label=u'weapons',
+        )
+        self.dropdown_fields.items.create(key=u'gun', value=u'eagle')
+        self.dropdown_fields.items.create(key=u'sword', value=u'excalibur')
+        serializer = FormidableSerializer(instance=self.form, data=self.data)
+        self.assertTrue(serializer.is_valid())
+        form = serializer.save()
+        self.assertEquals(form.pk, self.form.pk)
+        self.assertEquals(form.fields.count(), 0)
+
+    def test_delete_items_on_update(self):
+        self.dropdown_fields = self.form.fields.create(
+            slug='dropdown-input', type_id='dropdown', label=u'weapons',
+        )
+        self.dropdown_fields.accesses.create(
+            access_id='padawan', level='required'
+        )
+        self.dropdown_fields.items.create(key=u'gun', value=u'eagle')
+        self.dropdown_fields.items.create(key=u'sword', value=u'excalibur')
+        data = copy.deepcopy(self.data)
+        data['fields'] = copy.deepcopy(self.fields_items)
+        data['fields'][0]['items'] = {}
+        serializer = FormidableSerializer(instance=self.form, data=data)
+        self.assertTrue(serializer.is_valid())
+        form = serializer.save()
+        self.assertEquals(form.pk, self.form.pk)
+        self.assertEquals(form.fields.count(), 1)
+        field = form.fields.first()
+        self.assertEquals(field.items.count(), 0)
---
+++ formidable/tests/tests_integration.py
@@ -0,0 +1,199 @@
+# -*- coding: utf-8 -*-
+from copy import deepcopy
+
+from django.core.urlresolvers import reverse
+from django.conf import settings
+
+from rest_framework.test import APITestCase
+
+from formidable.models import Formidable
+
+form_data = {
+    "label": "test create",
+    "description": "my first formidable by api",
+    "fields": [
+        {
+            "label": "hello",
+            "slug": "textslug",
+            "type_id": "text",
+            "placeholder": None,
+            "helptext": None,
+            "default": None,
+            "accesses": [
+                {"access_id": "padawan", "level": "required"},
+                {"access_id": "jedi", "level": "editable"},
+                {"access_id": "jedi-master", "level": "readonly"},
+                {"access_id": "human", "level": "hidden"},
+            ]
+        },
+    ]
+}
+
+form_data_items = {
+    "label": "test create",
+    "description": "my first formidable by api",
+    "fields": [{
+        "label": "my_dropdwon",
+        "slug": "dropdown_slug",
+        "type_id": "dropdown",
+        "placeholder": None,
+        "helptext": "Lesfrites c'est bon",
+        "default": None,
+        "accesses": [
+            {"access_id": "padawan", "level": "required"},
+            {"access_id": "jedi", "level": "editable"},
+            {"access_id": "jedi-master", "level": "readonly"},
+            {"access_id": "human", "level": "hidden"},
+        ],
+        "items": {
+            "plop": "coin",
+            "tuto": "toto"
+        },
+        "multiple": False
+    }]
+}
+
+
+class CreateFormTestCase(APITestCase):
+
+    def test_simple(self):
+        initial_count = Formidable.objects.count()
+        res = self.client.post(
+            reverse('formidable:form_create'), form_data, format='json'
+        )
+        self.assertEquals(res.status_code, 201)
+        self.assertEquals(initial_count + 1, Formidable.objects.count())
+        formidable = Formidable.objects.order_by('pk').last()
+        self.assertEquals(formidable.fields.count(), 1)
+        field = formidable.fields.first()
+        self.assertEquals(field.accesses.count(), 4)
+        accesses = [
+            ('padawan', 'required'), ('jedi', 'editable'),
+            ('jedi-master', 'readonly'), ('human', 'hidden'),
+        ]
+        for access, level in accesses:
+            self.assertTrue(
+                field.accesses.filter(access_id=access, level=level).exists()
+            )
+
+    def test_with_items_in_fields(self):
+        initial_count = Formidable.objects.count()
+        res = self.client.post(
+            reverse('formidable:form_create'), form_data_items, format='json'
+        )
+        self.assertEquals(res.status_code, 201)
+        self.assertEquals(initial_count + 1, Formidable.objects.count())
+        formidable = Formidable.objects.order_by('pk').last()
+        self.assertEquals(formidable.fields.count(), 1)
+        field = formidable.fields.first()
+        self.assertEquals(field.items.count(), 2)
+
+    def test_forgotten_items_fields(self):
+        form_data_without_items = deepcopy(form_data_items)
+        form_data_without_items['fields'][0].pop('items')
+        res = self.client.post(
+            reverse('formidable:form_create'), form_data_without_items,
+            format='json'
+        )
+        self.assertEquals(res.status_code, 400)
+
+
+class UpdateFormTestCase(APITestCase):
+
+    def setUp(self):
+        super(UpdateFormTestCase, self).setUp()
+        self.form = Formidable.objects.create(
+            label=u'test', description='test'
+        )
+
+    @property
+    def edit_url(self):
+        return reverse(
+            'formidable:form_detail', args=[self.form.id]
+        )
+
+    def test_simple(self):
+        initial_count = Formidable.objects.count()
+        data = {
+            'label': 'edited label',
+            'description': 'edited description',
+            'fields': []
+        }
+        res = self.client.put(self.edit_url, data)
+        self.assertEquals(res.status_code, 200)
+        formidable = Formidable.objects.order_by('pk').last()
+        self.assertEquals(formidable.pk, self.form.pk)
+        self.assertEquals(formidable.label, u'edited label')
+        self.assertEquals(formidable.description, u'edited description')
+        self.assertEquals(Formidable.objects.count(), initial_count)
+
+    def test_update_simple_fields(self):
+        field = self.form.fields.create(
+            type_id='text', slug='textslug', label=u'mytext',
+        )
+        for access in settings.FORMIDABLE_ACCESSES:
+            field.accesses.create(access_id=access, level=u'editable')
+        res = self.client.put(self.edit_url, form_data,  format='json')
+        self.assertEquals(res.status_code, 200)
+        form = Formidable.objects.order_by('pk').last()
+        self.assertEquals(form.pk, self.form.pk)
+        self.assertEquals(form.fields.count(), 1)
+        field = form.fields.first()
+        self.assertEquals(field.label, u'hello')
+        self.assertEquals(field.accesses.count(), 4)
+
+    def test_create_field_on_update(self):
+        field = self.form.fields.create(
+            type_id='text', slug='textslug', label=u'mytext',
+        )
+        for access in settings.FORMIDABLE_ACCESSES:
+            field.accesses.create(access_id=access, level=u'editable')
+
+        data = deepcopy(form_data)
+        data['fields'].extend(form_data_items['fields'])
+        res = self.client.put(self.edit_url, data,  format='json')
+        self.assertEquals(res.status_code, 200)
+        form = Formidable.objects.order_by('pk').last()
+        self.assertEquals(form.pk, self.form.pk)
+        self.assertEquals(form.fields.count(), 2)
+
+    def test_delete_field_on_update(self):
+        self.form.fields.create(
+            type_id='text', slug='textslug', label=u'mytext',
+        )
+        self.form.fields.create(
+            type_id='text', slug='delete-slug', label='text'
+        )
+
+        for access in settings.FORMIDABLE_ACCESSES:
+            for field in self.form.fields.all():
+                field.accesses.create(access_id=access, level=u'editable')
+
+        res = self.client.put(self.edit_url, form_data,  format='json')
+        self.assertEquals(res.status_code, 200)
+        form = Formidable.objects.order_by('pk').last()
+        self.assertEquals(form.pk, self.form.pk)
+        self.assertEquals(form.fields.count(), 1)
+        field = form.fields.first()
+        self.assertEquals(field.label, u'hello')
+        self.assertEquals(field.accesses.count(), 4)
+        self.assertTrue(field.accesses.filter(
+            access_id=u'padawan', level='required'
+        ).exists())
+        self.assertTrue(field.accesses.filter(
+            access_id=u'human', level='hidden'
+        ).exists())
+        self.assertTrue(field.accesses.filter(
+            access_id="jedi-master", level="readonly"
+        ).exists())
+
+
+class TestAccess(APITestCase):
+
+    def test_get(self):
+        response = self.client.get(reverse('formidable:accesses_list'))
+        self.assertEquals(response.status_code, 200)
+        self.assertEquals(len(response.data), 4)
+        for access in response.data:
+            self.assertIn('id', access)
+            self.assertIn(access['id'], settings.FORMIDABLE_ACCESSES)
