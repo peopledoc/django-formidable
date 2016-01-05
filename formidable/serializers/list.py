cbaf916f7f6ae705e76f5b0f45f53bcd9cb973a1
Guillaume Camera <guillaume.camera@novapost.fr>
2016-01-05 16:15:38 +0100

    refs #20, first implementation of NestedListSerializer

---
+++ formidable/serializers/list.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+import logging
+
+from rest_framework.serializers import ListSerializer
+
+logger = logging.getLogger(__name__)
+
+
+class NestedListSerializer(ListSerializer):
+
+    field_id = None
+    parent_name = None
+
+    def update(self, qs, validated_data, field):
+        created_ids, updated_ids, deleted_ids = self._extract_id(
+            qs, validated_data
+        )
+
+        qs.filter(**{self.field_id + '__in': list(deleted_ids)}).delete()
+
+        objects_list = list(qs.order_by(self.field_id).all())
+
+        ajust = 0
+
+        validated_data = sorted(validated_data, key=lambda x: x[self.field_id])
+
+        for index, data in enumerate(validated_data):
+
+            data[self.parent_name] = field.id
+            if data[self.field_id] in created_ids:
+                self.child.create(data)
+                ajust += 1
+            else:
+                self.child.update(objects_list[index-ajust], data)
+
+    def create(self, validated_data, field):
+
+        for data in validated_data:
+            data[self.parent_name] = field.id
+            self.child.create(data)
+
+    def _extract_id(self, qs, validated_data):
+        validated_ids = set([data[self.field_id] for data in validated_data])
+        db_ids = set([values[0] for values in qs.values_list(self.field_id)])
+        updated_ids = validated_ids & db_ids
+        created_ids = validated_ids - db_ids
+        deleted_ids = db_ids - validated_ids
+        return created_ids, updated_ids, deleted_ids
