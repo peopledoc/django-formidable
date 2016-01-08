75a231aef3ae54df1b5c6c0e215bf5d031a8bb72
Guillaume Camera <guillaume.camera@novapost.fr>
2016-01-08 15:25:40 +0100

    refs #32, forgotten glue file

---
+++ demo/demo/formidable_accesses.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from formidable.accesses import AccessObject
+
+
+def get_accesses():
+    return [
+        AccessObject(id=key, label=key)
+        for key in [u'padawan', 'jedi', 'jedi-master', 'human']
+    ]
