c04f37f3c6c0ba8c8de64ee475b3e210abfe2a65
Guillaume Camera <guillaume.camera@novapost.fr>
2016-01-11 15:50:34 +0100

    refs #29, added radios select

--- formidable/forms.py
+++ formidable/forms.py
@@ -14,7 +14,7 @@ class BaseDynamicForm(forms.Form):
 class FieldBuilder(object):
 
     widget_class = None
-    field_class = None
+    field_class = forms.CharField
 
     def __init__(self, field):
         self.field = field
@@ -28,13 +28,18 @@ class FieldBuilder(object):
         return self.field_class
 
     def get_field_kwargs(self):
+
         kwargs = {
             'required': self.get_required(),
             'label': self.get_label(),
+            'help_text': self.get_helptext(),
         }
+
         widget = self.get_widget()
+
         if widget:
             kwargs['widget'] = self.get_widget()
+
         return kwargs
 
     def get_widget(self):
@@ -54,8 +59,10 @@ class FieldBuilder(object):
             return False
 
         access = self.field.accesses.get(access_id=self.role)
+
         if access.level == u'HIDDEN':
             raise SkipField()
+
         return access.level == u'REQUIRED'
 
     def get_label(self):
@@ -66,21 +73,55 @@ class FieldBuilder(object):
 
 
 class TextFieldBuilder(FieldBuilder):
-
-    field_class = forms.CharField
+    pass
 
 
 class ParagraphFieldBuilder(FieldBuilder):
 
-    field_class = forms.CharField
     widget_class = forms.Textarea
+
+
+class CheckboxFieldBuilder(FieldBuilder):
+
+    widget_class = forms.CheckboxInput
+
+
+class ChoiceFieldBuilder(FieldBuilder):
+
+    field_class = forms.ChoiceField
+
+    def get_field_kwargs(self):
+        kwargs = super(ChoiceFieldBuilder, self).get_field_kwargs()
+        kwargs['choices'] = self.get_choices()
+        return kwargs
+
+    def get_choices(self):
+        return [(item.key, item.value) for item in self.field.items.all()]
+
+
+class DropdownFieldBuilder(ChoiceFieldBuilder):
+
+    widget_class = forms.Select
+
+    def get_widget_class(self):
+        if self.field.multiple:
+            return forms.SelectMultiple
+        return super(DropdownFieldBuilder, self).get_widget_class()
+
+
+class RadioFieldBuilder(ChoiceFieldBuilder):
+
+    widget_class = forms.RadioSelect
 
 
 class FormFieldFactory(object):
 
     field_map = {
         'text': TextFieldBuilder,
-        'paragraph': ParagraphFieldBuilder
+        'paragraph': ParagraphFieldBuilder,
+        'dropdown': DropdownFieldBuilder,
+        'checkbox': CheckboxFieldBuilder,
+        'radios': RadioFieldBuilder,
     }
 
     @classmethod
--- formidable/tests/test_forms.py
+++ formidable/tests/test_forms.py
@@ -14,28 +14,21 @@ class TestDynamicForm(TestCase):
         self.text_field = self.form.fields.create(
             slug=u'text-input', type_id=u'text', label=u'mytext'
         )
-        self.text_field.accesses.create(access_id=u'padawan',
-                                        level=u'REQUIRED')
-        self.text_field.accesses.create(access_id=u'jedi',
-                                        level=u'EDITABLE')
-        self.text_field.accesses.create(access_id=u'human',
-                                        level=u'HIDDEN')
 
     def test_text_input(self):
-        form_class = self.form.get_django_form_class(role=u'padawan')
+        form_class = self.form.get_django_form_class()
         form = form_class()
         self.assertIn('text-input', form.fields)
         text = form.fields['text-input']
-        self.assertEquals(text.required, True)
+        self.assertEquals(text.required, False)
         self.assertEquals(type(text), forms.CharField)
         self.assertEquals(type(text.widget), forms.TextInput)
 
     def test_paragraph_input(self):
-        paragraph = self.form.fields.create(
+        self.form.fields.create(
             slug=u'area-input', type_id=u'paragraph', label=u'type a msg'
         )
-        paragraph.accesses.create(access_id=u'padawan', level=u'EDITABLE')
-        form_class = self.form.get_django_form_class(role=u'padawan')
+        form_class = self.form.get_django_form_class()
         form = form_class()
         self.assertIn('area-input', form.fields)
         text = form.fields['area-input']
@@ -43,7 +36,41 @@ class TestDynamicForm(TestCase):
         self.assertEquals(type(text), forms.CharField)
         self.assertEquals(type(text.widget), forms.Textarea)
 
+    def test_dropdown_input(self):
+        drop = self.form.fields.create(
+            slug=u'weapons', type_id=u'dropdown', label=u'chose you weapon'
+        )
+        for key in ['sword', 'gun']:
+            drop.items.create(key=key, value=key)
+
+        form_class = self.form.get_django_form_class()
+        form = form_class()
+        self.assertIn('weapons', form.fields)
+        dropdown = form.fields['weapons']
+        self.assertEquals(type(dropdown), forms.ChoiceField)
+        self.assertEquals(type(dropdown.widget), forms.Select)
+        self.assertTrue(dropdown.choices)
+        self.assertEquals(len(dropdown.choices), 2)
+
+    def test_radio_input(self):
+        field = self.form.fields.create(
+            slug=u'input-radio', type_id=u'checkbox',
+            label=u'chose you weapon'
+        )
+        for key in ['sword', 'gun']:
+            field.items.create(key=key, value=key)
+
+        form_class = self.form.get_django_form_class()
+        form = form_class()
+        self.assertIn('input-radio', form.fields)
+        checkbox = form.fields['input-radio']
+        self.assertEquals(type(checkbox), forms.ChoiceField)
+        self.assertEquals(type(checkbox.widget), forms.RadioSelect)
+        self.assertTrue(checkbox.choices)
+        self.assertEquals(len(checkbox.choices), 2)
+
     def test_hidden_field(self):
+        self.text_field.accesses.create(access_id=u'human', level=u'HIDDEN')
         form_class = self.form.get_django_form_class(role=u'human')
         form = form_class()
         self.assertNotIn('text-input', form.fields)
