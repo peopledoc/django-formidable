8f841e9b8d4a967cadafadbede7727cfa6e037b2
GreatWizard <guillaume.gerard88@gmail.com>
2016-01-26 20:03:22 +0100

    Better regexp for roles in the demo url

--- demo/demo/builder/urls.py
+++ demo/demo/builder/urls.py
@@ -7,7 +7,7 @@ from demo.builder.views import (
 urlpatterns = patterns(
     r'',
     url(r'^$', FormidableListView.as_view(), name='formidable-list'),
-    url(r'^(?P<pk>\d+)/(?P<role>\w+)/$', FormidableDetailView.as_view(),
+    url(r'^(?P<pk>\d+)/(?P<role>[-_\w]+)/$', FormidableDetailView.as_view(),
         name='formidable-detail'),
     url(r'^builder/(?P<pk>\d+)$', FormidableBuilderView.as_view(),
         name='formidable-builder'),
