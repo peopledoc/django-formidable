88fda2cbbc55c9cba4405ec96247e6476fa14b23
GreatWizard <guillaume.gerard88@gmail.com>
2016-01-26 18:35:44 +0100

    Add roles in links into formidable_list's template

--- demo/demo/builder/views.py
+++ demo/demo/builder/views.py
@@ -2,11 +2,17 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from formidable.models import Formidable
+from formidable.accesses import get_accesses
 
 
 class FormidableListView(ListView):
 
     model = Formidable
+
+    def get_context_data(self):
+        context = super(FormidableListView, self).get_context_data()
+        context['roles'] = get_accesses()
+        return context
 
 
 class FormidableDetailView(DetailView):
