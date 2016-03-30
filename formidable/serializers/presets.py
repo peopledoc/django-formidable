7a56d8d189f5a24099c51411659e7a4c7ffe56dd
Guillaume Camera <guillaume.camera@novapost.fr>
2016-03-30 18:16:55 +0200

    refs #60, added a listserializer for argument

--- formidable/serializers/presets.py
+++ formidable/serializers/presets.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from rest_framework.serializers import Serializer
+from rest_framework.serializers import Serializer, ListSerializer
 from rest_framework import fields
+
+from formidable.forms.validations.presets import PresetArgument
 
 
 class ClassAttrSerializer(object):
@@ -27,7 +29,21 @@ class ListField(fields.Field):
         return list(value)
 
 
+class PresetArgsSerializerList(ListSerializer):
+
+    def get_attribute(self, instance):
+        res = []
+        for slug, arg in instance.__class__.MetaParameters.__dict__.items():
+            if isinstance(arg, PresetArgument):
+                arg.set_slug(slug)
+                res.append(arg)
+        return res
+
+
 class PresetsArgsSerializer(Serializer):
+
+    class Meta:
+        list_serializer_class = PresetArgsSerializerList
 
     slug = fields.SlugField()
     label = fields.CharField()
@@ -42,4 +58,4 @@ class PresetsSerializer(Serializer):
     label = CharFieldClassAttr()
     description = CharFieldClassAttr()
     message = CharFieldClassAttr(source='default_message')
-#    fields = PresetsArgsSerializer(many=True)
+    fields = PresetsArgsSerializer(many=True)
