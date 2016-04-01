e93559dd35d17c3b40226fb72202bed297cfd1e8
Guillaume Camera <guillaume.camera@novapost.fr>
2016-04-01 11:18:18 +0200

    refs #60, added serializer preset to form serializer

--- formidable/serializers/forms.py
+++ formidable/serializers/forms.py
@@ -5,17 +5,19 @@ from rest_framework import serializers
 from formidable.models import Formidable
 from formidable.serializers import fields
 from formidable.serializers.common import WithNestedSerializer
+from formidable.serializers.presets import PresetModelSerializer
 
 
 class FormidableSerializer(WithNestedSerializer):
 
     fields = fields.FieldidableSerializer(many=True)
+    presets = PresetModelSerializer(many=True)
 
-    nested_objects = ['fields']
+    nested_objects = ['fields', 'presets']
 
     class Meta:
         model = Formidable
-        fields = ('label', 'description', 'fields', 'id')
+        fields = ('label', 'description', 'fields', 'id', 'presets')
         depth = 2
         extra_kwargs = {'id': {'read_only': True}}
 
