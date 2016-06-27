5fb655325a3dcdba12ed6fca64261004124d8560
Nicolas Dubois <nicolas.c.dubois@gmail.com>
2016-06-27 14:35:14 +0200

    Remove unused import
    `CommandError` was not used in the management command.

--- demo/demo/management/commands/populate.py
+++ demo/demo/management/commands/populate.py
@@ -1,4 +1,4 @@
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from formidable.serializers.forms import FormidableSerializer
 import json
 
