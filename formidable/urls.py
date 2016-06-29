be3fe04fb86f6a3d87145b9fcc12492c27da1904
Guillaume Camera <guillaume.camera@novapost.fr>
2016-06-29 15:55:00 +0200

    refs #80, added view in order to validate data from API
    The view is here in order to help the front to validate data. The
    complex validation done through presets cannot be running on the front
    side. The view return a 204 , emtyp content, when the data are correct,
    else returns 400 with the errors dictionary taken from the django form
    The viewi is plugged in the set of API view available for the front
    view.

--- formidable/urls.py
+++ formidable/urls.py
@@ -2,12 +2,14 @@ from django.conf.urls import patterns, url
 
 from formidable.views import FormidableDetail, FormidableCreate, AccessList
 from formidable.views import ContextFormDetail
-from formidable.views import PresetsList
+from formidable.views import PresetsList, ValidateView
 
 urlpatterns = patterns(
     r'',
     url(r'^forms/(?P<pk>\d+)/$', ContextFormDetail.as_view(),
         name='form_detail'),
+    url(r'^forms/(?P<pk>\d+)/validate/$', ValidateView.as_view(),
+        name='form_validation'),
     url(r'^builder/forms/(?P<pk>\d+)/$', FormidableDetail.as_view(),
         name='form_detail'),
     url(r'^builder/forms/$', FormidableCreate.as_view(), name='form_create'),
