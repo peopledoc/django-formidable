cdb2f9604f8a722da840989d797df7270a74e94e
Guillaume Camera <guillaume.camera@novapost.fr>
2016-07-14 20:50:34 +0200

    refs #108, Add edition mode in python builder

--- formidable/forms/__init__.py
+++ formidable/forms/__init__.py
@@ -6,6 +6,7 @@ from a formidable object.
 Given a formidable object, you can use :func:`get_dynamic_form_class` to get
 its corresponding django form class.
 """
+from __future__ import unicode_literals
 from collections import OrderedDict
 
 from django import forms
@@ -85,7 +86,7 @@ def get_dynamic_form_class(formidable, role=None, field_factory=None):
             attrs[field.slug] = form_field
 
     attrs['rules'] = presets_register.build_rules(formidable)
-    return type('DynamicForm', (BaseDynamicForm,), attrs)
+    return type(str('DynamicForm'), (BaseDynamicForm,), attrs)
 
 
 class FormidableForm(forms.Form):
@@ -113,10 +114,34 @@ class FormidableForm(forms.Form):
     """
 
     @classmethod
-    def to_formidable(cls, label, description=u''):
-        form = Formidable.objects.create(label=label, description=description)
+    def to_formidable(cls, label=None, description=None, instance=None):
+        if not instance:
+            if not label:
+                raise ValueError("On creation mode, label value is mandatory.")
+            description = description or ''
+            form = Formidable.objects.create(
+                label=label, description=description
+            )
+        else:
+            form = Formidable.objects.get(pk=instance.pk)
+            form = cls.clean_form(form, label, description)
+
         order = 0
         for slug, field in cls.declared_fields.items():
             field.to_formidable(form, order, slug)
             order += 1
+        return form
+
+    @classmethod
+    def clean_form(cls, form, label, description):
+        form.presets.all().delete()
+        form.fields.all().delete()
+        if description or label:
+            kwargs = {
+                'description': description or form.description,
+                'label': label or form.label,
+            }
+            Formidable.objects.filter(pk=form.pk).update(**kwargs)
+            form.refresh_from_db()
+
         return form
