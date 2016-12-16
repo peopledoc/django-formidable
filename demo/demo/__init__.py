0e8b5e1bff6879f71f696e464b0e1ca12626240a
Bruno Bord <bruno.bord@novapost.fr>
2016-12-16 10:54:25 +0100

    refs #134 -- failing tests for post-save callback on form create

---
+++ demo/demo/__init__.py
@@ -0,0 +1,7 @@
+def callback_save(*args, **kwargs):
+    """
+    This function will be called as a dummy callback on form post-save/create.
+
+    It doesn't do much, and that's fine.
+    """
+    return True
