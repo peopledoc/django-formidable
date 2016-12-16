a81cf2b45e8581f733380d2e525ec27fb3440f63
Bruno Bord <bruno.bord@novapost.fr>
2016-12-16 14:43:38 +0100

    refs #134 -- (failing) test for post_save actions that raise exceptions

--- demo/demo/__init__.py
+++ demo/demo/__init__.py
@@ -1,3 +1,8 @@
+
+class DemoCallbackException(Exception):
+    "A dummy callback exception"
+
+
 def callback_save(*args, **kwargs):
     """
     This function will be called as a dummy callback on form post-save/create.
@@ -5,3 +10,12 @@ def callback_save(*args, **kwargs):
     It doesn't do much, and that's fine.
     """
     return True
+
+
+def callback_exception(*args, **kwargs):
+    """
+    This function will be called-back on form post-save/create
+
+    It'll raise an exception
+    """
+    raise DemoCallbackException()
