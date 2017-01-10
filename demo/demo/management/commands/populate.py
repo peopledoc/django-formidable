ede9477a1ab4a83e241add8e3f3f6061cde0a3ce
Bruno Bord <bruno.bord@novapost.fr>
2017-01-10 16:55:05 +0100

    Upsert mode for populate makefile target
    Now, by default, the "populate" commands will update an existing form (i.e. if the program can find the Formidable instance in the DB, or if the option ``--append-only`` is not set).
    If the object can't be found, it'll be created.

--- demo/demo/management/commands/populate.py
+++ demo/demo/management/commands/populate.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand
 from formidable.serializers.forms import FormidableSerializer
+from formidable.models import Formidable
 import json
 
 
@@ -8,15 +9,34 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('filename', nargs='+', type=str)
+        parser.add_argument('--append', action="store_true", default=False)
 
     def handle(self, *args, **options):
         for filename in options['filename']:
             with open(filename) as f:
                 data = json.load(f)
+
+                # By default, the serializer is for updating
                 serializer = FormidableSerializer(data=data)
+                if not options['append'] and 'id' in data:
+                    try:
+                        instance = Formidable.objects.get(id=data['id'])
+                        # If the instance is found
+                        # passing it will trigger an "update"
+                        serializer = FormidableSerializer(
+                            instance,
+                            data=data
+                        )
+                    except Formidable.DoesNotExist:
+                        self.stdout.write(
+                            'Unknown Formidable form with pk: `%s`; creating'
+                            % data['id'])
+
                 if serializer.is_valid():
                     serializer.save()
-                    self.stdout.write('Successfully populate "%s"' % filename)
+                    self.stdout.write(
+                        'Successfully populate "%s (%d)"'
+                        % (serializer.instance, serializer.instance.pk))
                 else:
                     self.stdout.write('Broken "%s" %s' % (
                         filename, serializer.errors))
