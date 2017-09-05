216f51f889eeb2674cb88b45c66ea2e91a2136db
Bruno Bord <bruno.bord@novapost.fr>
2017-09-05 10:06:16 +0200

    refs #255 -- drop preset tables

---
+++ formidable/migrations/0006_drop_preset_tables.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('formidable', '0005_conditions_default'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='preset',
+            name='form',
+        ),
+        migrations.RemoveField(
+            model_name='presetarg',
+            name='preset',
+        ),
+        migrations.DeleteModel(
+            name='PresetArg',
+        ),
+        migrations.DeleteModel(
+            name='Preset',
+        ),
+    ]
