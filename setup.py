8ec1bee61df7454fb2c4cb5c6a2c46e4d76f7d47
Bruno Bord <bruno.bord@people-doc.com>
2017-10-09 11:17:20 +0200

    fixup! fixup! Django Formidable is compatible with DRF<=3.6 only

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
                  'guillaume.gerard@people-doc.com',
     install_requires=[
         'Django',
-        'djangorestframework',
+        'djangorestframework<3.7',
         'markdown',
         'python-dateutil',
         'jsonfield',
