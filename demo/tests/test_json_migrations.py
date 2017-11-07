37c3dd396fea92426314bc47208477a4be6b7bd3
Nicolas Dubois <nicolas.c.dubois@gmail.com>
2017-11-07 01:02:47 +0100

    Refactor test for JSON migrations
    Instead of using deepcopy(), we now used a small fixture method that
    returns data.
    
    Helped-by: @alexdashkov

--- demo/tests/test_json_migrations.py
+++ demo/tests/test_json_migrations.py
@@ -1,6 +1,5 @@
 import mock
 import os
-from copy import deepcopy
 
 from django.test import TestCase
 
@@ -12,18 +11,21 @@ from formidable.json_migrations import migrate
 @mock.patch('formidable.json_migrations.package',
             'tests.json_migrations')
 class JSONMigrationTestCase(TestCase):
-    def setUp(self):
-        super(JSONMigrationTestCase, self).setUp()
-
-        self.data = deepcopy({
+    def _fixtures(self, version=None):
+        data = {
             'fields': [
                 {'foo': 'bar', 'help_text': 'SOS'},
                 {'foo': 'baz', 'help_text': 'SAS'},
             ]
-        })
+        }
+        if version:
+            data['version'] = version
+
+        return data
 
     def test_migration_from_scratch(self):
-        updated_data, version = migrate(self.data)
+        data = self._fixtures()
+        updated_data = migrate(data)
 
         self.assertEqual(updated_data, {
             'version': 2,
@@ -32,10 +34,11 @@ class JSONMigrationTestCase(TestCase):
                 {'foo': 'baz', 'description': 'SAS'},
             ]
         })
-        self.assertEqual(version, 2)
+        self.assertEqual(updated_data['version'], 2)
 
     def test_migration_from_version_1(self):
-        updated_data, version = migrate(self.data, 1)
+        data = self._fixtures(version=1)
+        updated_data = migrate(data, 1)
 
         self.assertEqual(updated_data, {
             'version': 2,
@@ -44,30 +47,33 @@ class JSONMigrationTestCase(TestCase):
                 {'foo': 'baz', 'help_text': 'SAS'},
             ],
         })
-        self.assertEqual(version, 2)
+        self.assertEqual(updated_data['version'], 2)
 
     def test_migration_from_version_2(self):
-        updated_data, version = migrate(self.data, 2)
-        self.assertEqual(updated_data, self.data)
-        self.assertEqual(version, 2)
+        data = self._fixtures(version=2)
+        updated_data = migrate(data, 2)
+        self.assertEqual(updated_data, data)
 
     @mock.patch('tests.json_migrations.0002_add_version.migrate')
     @mock.patch('tests.json_migrations.0001_rename_helptext.migrate')
     def test_migration_from_scratch_call_count(self, migrate_1, migrate_2):
-        migrate(self.data)
+        data = self._fixtures()
+        migrate(data)
         self.assertEqual(migrate_1.call_count, 1)
         self.assertEqual(migrate_2.call_count, 1)
 
     @mock.patch('tests.json_migrations.0002_add_version.migrate')
     @mock.patch('tests.json_migrations.0001_rename_helptext.migrate')
     def test_migration_from_version_1_call_count(self, migrate_1, migrate_2):
-        migrate(self.data, 1)
+        data = self._fixtures(version=1)
+        migrate(data, 1)
         self.assertEqual(migrate_1.call_count, 0)
         self.assertEqual(migrate_2.call_count, 1)
 
     @mock.patch('tests.json_migrations.0002_add_version.migrate')
     @mock.patch('tests.json_migrations.0001_rename_helptext.migrate')
     def test_migration_from_version_2_call_count(self, migrate_1, migrate_2):
-        migrate(self.data, 2)
+        data = self._fixtures(version=2)
+        migrate(data, 2)
         self.assertEqual(migrate_1.call_count, 0)
         self.assertEqual(migrate_2.call_count, 0)
