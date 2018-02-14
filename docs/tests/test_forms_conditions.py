fdb378796eca78223413c465bc02b1dafa2e0a9a
Bruno Bord <bruno.bord@people-doc.com>
2018-02-14 15:59:40 +0100

    Fixed required properties for Condition entities

--- docs/tests/test_forms_conditions.py
+++ docs/tests/test_forms_conditions.py
@@ -30,20 +30,24 @@ def _load_20_simple_condition():
 
 def test_simple_condition_validation_no_name():
     form, no_name = _load_20_simple_condition()
-    # FIXME: this should fail validation ; name should be mandatory.
     del no_name['name']
     form['conditions'] = [no_name]
     errors = sorted(validator.iter_errors(form), key=lambda e: e.path)
-    assert len(errors) == 0
+    assert len(errors) == 1
+    error = errors[0]
+    assert error.validator == 'required'
+    assert error.message == "'name' is a required property"
 
 
 def test_simple_condition_validation_no_fields():
     form, no_fields = _load_20_simple_condition()
-    # FIXME: this should fail validation ; field IDs should be mandatory.
     del no_fields['field_ids']
     form['conditions'] = [no_fields]
     errors = sorted(validator.iter_errors(form), key=lambda e: e.path)
-    assert len(errors) == 0
+    assert len(errors) == 1
+    error = errors[0]
+    assert error.validator == 'required'
+    assert error.message == "'field_ids' is a required property"
 
 
 def test_simple_condition_validation_fields_empty():
@@ -57,11 +61,13 @@ def test_simple_condition_validation_fields_empty():
 
 def test_simple_condition_validation_no_tests():
     form, no_tests = _load_20_simple_condition()
-    # FIXME: this should fail validation ; tests should be mandatory.
     del no_tests['tests']
     form['conditions'] = [no_tests]
     errors = sorted(validator.iter_errors(form), key=lambda e: e.path)
-    assert len(errors) == 0
+    assert len(errors) == 1
+    error = errors[0]
+    assert error.validator == 'required'
+    assert error.message == "'tests' is a required property"
 
 
 def test_simple_condition_validation_empty_tests():
@@ -75,11 +81,13 @@ def test_simple_condition_validation_empty_tests():
 
 def test_simple_condition_validation_no_action():
     form, no_action = _load_20_simple_condition()
-    # FIXME: this should fail validation ; it should have an action property.
     del no_action['action']
     form['conditions'] = [no_action]
     errors = sorted(validator.iter_errors(form), key=lambda e: e.path)
-    assert len(errors) == 0
+    assert len(errors) == 1
+    error = errors[0]
+    assert error.validator == 'required'
+    assert error.message == "'action' is a required property"
 
 
 def _load_20_simple_condition_test():
