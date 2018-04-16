bcca83341fc1e15b07d500e5c79844ea8630239c
Sylvain Delabye <sylvain.delabye@people-doc.com>
2018-04-16 15:07:39 +0200

    Add a deprecation warning for django 1.8 and 1.9

--- formidable/app.py
+++ formidable/app.py
@@ -6,6 +6,10 @@ The :class:`FormidableConfig` checks various configuration settings:
 * post-update and post-create callbacks
 """
 
+import warnings
+from distutils.version import StrictVersion
+
+import django
 from django.apps import AppConfig
 
 
@@ -21,3 +25,9 @@ class FormidableConfig(AppConfig):
         """
         from .views import check_callback_configuration
         check_callback_configuration()
+
+        # Deprecation warning for django 1.8 and 1.9
+        if StrictVersion(django.get_version()) < StrictVersion("1.10"):
+            warnings.warn("Support for Django 1.8 and 1.9 is about to be "
+                          "dropped. Please update to a newer version.",
+                          DeprecationWarning)
