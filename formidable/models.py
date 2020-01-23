f58170269c091d2ad9795e95d5e058ef1466bab3
Bruno Bord <brunobord@gmail.com>
2020-01-23 12:20:38 +0100

    Revert "Integrate jsonfield2 instead of jsonfield"

--- formidable/models.py
+++ formidable/models.py
@@ -4,7 +4,7 @@ from django.utils.encoding import python_2_unicode_compatible
 
 from formidable import constants
 from formidable.register import FieldSerializerRegister
-from jsonfield import JSONField
+from jsonfield.fields import JSONField
 
 
 @python_2_unicode_compatible
